fc182dc38130c856dca4160c05fe0affa555213e
root <root@kali.com>
2026-05-07 22:07:44 +0530

    add robustscaler

--- train.py
+++ train.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import RobustScaler
 import pickle
 
-# Load dataset
+
 df = pd.read_csv("House Price Prediction Dataset.csv")
 df = df.dropna()
 
-# One-hot encoding 
 df = pd.get_dummies(df, columns=['Location', 'Condition', 'Garage'])
 
+# Features and target
 X = df.drop("Price", axis=1)
 y = df["Price"]
 
@@ -17,10 +18,22 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = RandomForestRegressor(n_estimators=300, random_state=42)
-model.fit(X_train, y_train)
+# Apply RobustScaler
+scaler = RobustScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Train model
+model = RandomForestRegressor(
+    n_estimators=300,
+    random_state=42
+)
+
+model.fit(X_train_scaled, y_train)
 
 pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(scaler, open("scaler.pkl", "wb"))
 pickle.dump(X.columns, open("columns.pkl", "wb"))
 
-print("✅ Model trained successfully!")
+print("Model trained successfully!")
